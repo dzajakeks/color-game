be7cb30db7e0daee8309a85b0b81aba7a6c9c3bd
Marko Popovic <mare17p@gmail.com>
2023-10-07 01:22:45 +0200

    title change

---
+++ src/MatchingColor.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+
+const MatchingColor = ({
+  matchingColor,
+  setRed,
+  setGreen,
+  setBlue,
+  red,
+  green,
+  blue,
+  randomColor,
+}) => {
+  return (
+    <form>
+      <div className='coloredSquares'>
+        <div
+          style={{
+            background: matchingColor,
+            width: '60px',
+            height: '60px',
+            border: '1px solid black',
+            borderRadius: '5px',
+          }}
+        ></div>
+        <div
+          style={{
+            background: randomColor,
+            width: '60px',
+            height: '60px',
+            border: '1px solid black',
+            borderRadius: '5px',
+          }}
+        ></div>
+      </div>
+      <div>
+        <input
+          value={red}
+          onChange={(e) => {
+            setRed(e.target.value);
+          }}
+          className='redSlider'
+          type='range'
+          min='0'
+          max='255'
+        />
+      </div>
+      <div>
+        <input
+          value={green}
+          onChange={(e) => {
+            setGreen(e.target.value);
+          }}
+          className='greenSlider'
+          type='range'
+          min='0'
+          max='255'
+        />
+        <div>
+          <input
+            value={blue}
+            onChange={(e) => {
+              setBlue(e.target.value);
+            }}
+            className='blueSlider'
+            type='range'
+            min='0'
+            max='255'
+          />
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default React.memo(MatchingColor);
---
+++ src/RandomColor.jsx
@@ -0,0 +1,16 @@
+// import React from 'react';
+
+// const RandomColor = ({ randomColor }) => {
+//   return (
+//     <div
+//       style={{
+//         background: randomColor,
+//         width: '60px',
+//         height: '60px',
+//         border: '1px solid black',
+//       }}
+//     ></div>
+//   );
+// };
+
+// export default RandomColor;
